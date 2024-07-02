32f7c03bc2986b264a13445b6751125fc1928462
B GOURAV <njrgourav@gmail.com>
2024-07-02 13:58:22 +0530

    Update FeatureSection.jsx

--- src/components/FeatureSection.jsx
+++ src/components/FeatureSection.jsx
@@ -11,7 +11,7 @@ const FeatureSection = () => {
         <h2 className="text-3xl sm:text-5xl lg:text-6xl mt-10 lg:mt-20 tracking-wide">
           Services {" "}
           <span className="bg-gradient-to-r from-blue-500 to-cyan-800 text-transparent bg-clip-text">
-            Offerred
+            Offered
           </span>
         </h2>
       </div>
